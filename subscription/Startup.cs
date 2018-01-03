57987e32fc635bb24e2e7c90aff1272d5244811c
Ohad <ohad.baruch@emc.com>
2018-01-03 16:02:30 +0200

    Initial commit

---
+++ subscription/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RawRabbit.vNext;
+using RawRabbit.vNext.Pipe;
+using RawRabbit.Configuration;
+using RawRabbit.Enrichers.GlobalExecutionId;
+using RawRabbit.Enrichers.MessageContext;
+using RawRabbit.Enrichers.MessageContext.Context;
+
+
+namespace subscription
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+            .AddRawRabbit(new RawRabbitOptions
+            {
+                ClientConfiguration =
+                    RawRabbit.Common
+                    .ConnectionStringParser.Parse("pibniaca:WbFVWQe0JkUyudLCMwrlmKVEZd08SlV7@golden-kangaroo.rmq.cloudamqp.com/pibniaca"),
+                Plugins = p => p.UseGlobalExecutionId().UseMessageContext<MessageContext>()
+            })
+            .AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
